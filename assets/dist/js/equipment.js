f5213a390d0f786f5e08105e971fb19655e6b5c5
SergAlKoba <Serg.Al.Koba@gmail.com>
2020-09-30 10:14:50 +0300

    new page equipment content

--- assets/dist/js/equipment.js
+++ assets/dist/js/equipment.js
@@ -1,8 +1,29 @@
 $(window).load(function(){
 
-    $('body').css({
-        'padding-top': headerHeight + 20,
-        'padding-bottom': footerHeight,
+    // $('body').css({
+    //     'padding-top': headerHeight + 20,
+    //     'padding-bottom': footerHeight,
+    // });
+
+    $('.video').on('click', function(){
+        $('.video-popup').addClass('active');
+        var video_data = $(this).data('src');
+        $('.video-popup iframe').attr('src', video_data);
+    });
+
+    $('.cloze').on('click', function(){
+        $('.video-popup').removeClass('active');
+        $('.video-popup iframe').attr('src', '');
+    });
+
+    $('.currency').find('li').on('click', function(){
+        var sun = $(this).data('sun');
+        var currency = $(this).data('currency');
+        $(this).parent('.currency').find('li').removeClass('active');
+        $(this).addClass('active');
+        console.log($(this).parent('.currency').prev('.buy-content').find('.uan span'));
+        $(this).parent('.currency').prev('.buy-content').find('.uan span').html(currency);
+        $(this).parent('.currency').prev('.buy-content').find('.uan strong').animateNumber({ number: sun });
     });
 
 });
